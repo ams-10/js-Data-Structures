cc70a3afc6360b31797582fa19a89ee7b960fed7
ams-10 <abdullahshareef123h@gmail.com>
2023-06-11 17:40:42 -0200

    completed linear search pending implementing map

---
+++ linearSearch.js
@@ -0,0 +1,15 @@
+let arr = [10, 20, 30, 40, 50];
+var x = 40;
+let len = arr.length;
+function search(arr ,x){
+    for(let i=0; i<len; i++){
+        if(arr[i] === x){
+            console.log(arr[i]+" "+i);
+            break;
+        }
+}
+}
+search(arr ,x);
+
+
+
