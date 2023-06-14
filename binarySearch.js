8d8c0b7a5687169b606e7adcd4de0d3bd07fbca1
ams-10 <abdullahshareef123h@gmail.com>
2023-06-14 17:48:52 -0200

    need to apply more logic and practice recursions and data structures along the way

--- binarySearch.js
+++ binarySearch.js
@@ -1,22 +1,48 @@
-let arr = [22, 10, 45, 78, 35, 21, 45, 66];
-let h = arr.length;
-let l = 0;
-function BinarySort(arr, l, h, x){
-    if(h >= l){
-        mid = Math.floor(h+l/2);
-    
-    if(arr[mid]==x){
-        return mid;
+// //program to demonstrate binary sort
+// let arr = [10, 48, 99, 73, 94, 44, 13, 45];
+// let high = arr.length;
+// let low = 0;
+
+// function BinarySort(arr, high, low, x){
+//     var mid = ((high + low) / 2);
+//     console.log("mid value : " +mid);
+//     mid = Math.floor(mid);
+//     if(high>low){
+//         if(arr[mid]===x){
+//             return mid;
+//         }
+//         else if(arr[mid] < x){
+//             BinarySort(arr, mid+1, high, x);
+//         }
+//         else if(arr[mid] > x){
+//             BinarySort(arr, low, mid-1, x);
+//         }
+//     }
+//     else{
+//         console.log("target value not present in array");
+//     }
+// }
+// BinarySort(arr, low, high, 73);
+let arr = [10, 48, 99, 73, 94, 44, 13, 45];
+let high = arr.length;
+let low = 0;
+
+function BinarySort(arr, high, low, x) {
+    var mid = Math.floor((high + low) / 2);
+    console.log("mid value: " + mid);
+
+    if (high > low) {
+        if (arr[mid] === x) {
+            return mid;
+        } else if (arr[mid] < x) {
+            return BinarySort(arr, high, mid + 1, x);
+        } else if (arr[mid] > x) {
+            return BinarySort(arr, mid - 1, low, x);
+        }
+    } else {
+        console.log("target value not present in array");
+        return -1; // Return a default value indicating the target value was not found
     }
-    if(arr[mid]>x){
-        return BinarySort(arr, l, mid-1, x);
-    }
-    return BinarySort(arr,mid+1, h, x);
-    
-    return -1;}
-    }
+}
 
-BinarySort(arr, 0, h-1, 21);
-
-
-
+console.log(BinarySort(arr, high, low, 44));
