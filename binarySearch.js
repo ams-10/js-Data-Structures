e1c47c991b694bc18a1eac69ba43d134fef685a9
ams-10 <abdullahshareef123h@gmail.com>
2023-06-15 14:02:12 -0200

    binary search completed

--- binarySearch.js
+++ binarySearch.js
@@ -1,28 +1,25 @@
 //program to demonstrate binary sort
-let arr = [10, 48, 99, 73, 41, 94, 44, 13, 45];
-arr.sort();
-console.log(arr);
+const arr = [10,20,22,36,47,88,95];
+console.log("\n" + arr +"\n");
 let high = arr.length;
+console.log("length of array is :" + high + "\n");
 let low = 0;
 
-function BinarySort(arr, high, low, x){
-    var mid = ((high + low) / 2);
-    console.log("mid value : " +mid);
-    mid = Math.floor(mid);
-    if(high>low){
-        if(arr[mid]===x){
+function binarySort(arr, low, high, x){
+    if(high < low || arr.length === 0) return -1;
+        let mid = low + Math.floor((high - low) / 2);
+        if(arr[mid] === x){
             return mid;
         }
-        else if(arr[mid] < x){
-            return BinarySort(arr, mid+1, high, x);
+        else if(x > arr[mid]){
+            return binarySort(arr, mid+1, high, x);
         }
-        else if(arr[mid] > x){
-            return BinarySort(arr, low, mid-1, x);4
+        else if (x <arr[mid]){
+            return binarySort(arr, low, mid-1, x);
         }
-    }
-    else{
-        console.log("target value not present in array");
-    }
+        else{
+            return console.log("value not found in array");
+        }
 }
-BinarySort(arr, low, high, 73);
 
+console.log("value present at index " + binarySort(arr, low, high, 95) + "\n");
